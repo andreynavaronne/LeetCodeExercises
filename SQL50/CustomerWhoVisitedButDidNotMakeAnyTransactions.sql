50f8e93e1a9ce83ab99bafcfe8a7dc0469178828
Andrey di Navaronne Gaudencio Leite <57904475+andreynavaronne@users.noreply.github.com>
2024-06-16 21:19:08 -0300

    Add files via upload

---
+++ SQL50/CustomerWhoVisitedButDidNotMakeAnyTransactions.sql
@@ -0,0 +1,2 @@
+# Write your MySQL query statement below
+SELECT customer_id, COUNT(v.visit_id) AS count_no_trans FROM Visits AS v LEFT JOIN Transactions AS t ON v.visit_id = t.visit_id WHERE t.transaction_id IS null GROUP BY v.customer_id
