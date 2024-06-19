516d2eae7358cb35d24e2e8f0af8cb0f202ba4a2
Andrey di Navaronne Gaudencio Leite <57904475+andreynavaronne@users.noreply.github.com>
2024-06-18 21:43:16 -0300

    Add files via upload

---
+++ SQL50/AverageSellingPrice.sql
@@ -0,0 +1,2 @@
+# Write your MySQL query statement below
+SELECT p.product_id, IF( SUM(p.price * u.units)/ SUM(u.units)  IS NULL, 0, ROUND(SUM(p.price * u.units)/ SUM(u.units) , 2)) AS average_price FROM Prices AS p LEFT JOIN UnitsSold AS u ON p.product_id = u.product_id AND u.purchase_date BETWEEN p.start_date AND p.end_date GROUP BY product_id
